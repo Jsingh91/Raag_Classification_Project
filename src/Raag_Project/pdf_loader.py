57e50f422c7e2947ee0c38790969fca53e77f99d
Jsingh91 <Jasneet942@gmail.com>
2025-12-06 14:52:03 -0500

    Wrote a second function to convert the pdf text to txt file and saved it in the pdf_loader.py file.

--- src/Raag_Project/pdf_loader.py
+++ src/Raag_Project/pdf_loader.py
@@ -1,5 +1,5 @@
 import pdfplumber
-
+# Load PDF
 def load_pdf_text(pdf_path):
     """
     Open a PDF, read all pages, and return one big string.
@@ -16,8 +16,18 @@ def load_pdf_text(pdf_path):
     full_text = "\n\n".join(all_pages_text)
     return full_text
 
-                
-                
+
+# PDF-Txt
+def get_txt_file(pdf_text):
+    """
+    Write the text of pdf passed and save as a txt file.
+    
+    """
+    txt_path = "../data/raw_text/Gurbani_with_index_Uni.txt"
+    with open(txt_path,"w", encoding="utf-8") as text_file:
+        text_file.write(pdf_text)
+    return txt_path
+    
 
         
         
